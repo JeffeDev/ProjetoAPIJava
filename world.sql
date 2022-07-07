408d24e396fd90d7b1ab1f369af61884f2741629
JeffeDev <jeffe.info@gmail.com>
2022-07-07 12:48:25 -0300

    alteracao pom.xml jacoco

---
+++ world.sql
@@ -0,0 +1,23 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.5
+-- Dumped by pg_dump version 11.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: my_db; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+--CREATE DATABASE clines WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
